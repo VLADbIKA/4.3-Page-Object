8a34edbd5469ce8a6c7131db5715d7f8f45307a6
VLADbIKA <94906381+VLADbIKA@users.noreply.github.com>
2022-02-21 14:29:24 +0500

    Delete pages directory

--- pages/basket_page.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from .locators import BasketPageLocators
-from .base_page import BasePage
-
-
-class BasketPage(BasePage):
-    def should_be_empty_basket(self):
-        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET), "'Empty basket' is not presented"
